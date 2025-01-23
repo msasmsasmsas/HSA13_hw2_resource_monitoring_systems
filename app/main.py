edff39ef4f280de1273b4cbec931a6a68a20e63c
HSA13_hw4_Stress_Testing_Approaches_and_tools <msas@ukr.net>
2025-01-23 22:24:16 +0200

    1

--- app/main.py
+++ app/main.py
@@ -10,7 +10,14 @@ db = client.test
 def health_check():
     return {"status": "ok"}
 
+#@app.get("/data")
+#def fetch_data():
+#    response = requests.get("http://elasticsearch:9200")
+#    return {"elasticsearch": response.json(), "mongo_collections": db.list_collection_names()}
 @app.get("/data")
 def fetch_data():
-    response = requests.get("http://elasticsearch:9200")
-    return {"elasticsearch": response.json(), "mongo_collections": db.list_collection_names()}
+    try:
+        response = requests.get("http://elasticsearch:9200", timeout=5)
+        return {"elasticsearch": response.json(), "mongo_collections": db.list_collection_names()}
+    except requests.exceptions.RequestException as e:
+        return {"error": str(e)}
